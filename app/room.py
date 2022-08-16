bc5050cc54130bd498ce05a77dd70affbe80cd18
KIMATA Tetsuya <kimata@green-rabbit.net>
2022-08-16 23:58:15 +0900

    fix: バスの指定は必要な時のみ行うようにした

--- app/room.py
+++ app/room.py
@@ -20,21 +20,29 @@ RASP_I2C_BUS_ARM = 0x1  # Raspberry Pi のデフォルトの I2C バス番号
 RASP_I2C_BUS_VC = 0x0  # dtparam=i2c_vc=on で有効化される I2C のバス番号
 
 SENSOR_MODULE_LIST = [
-    {"name": "scd4x", "bus": RASP_I2C_BUS_ARM},
-    {"name": "max31856", "bus": RASP_I2C_BUS_ARM},
-    {"name": "sht35", "bus": RASP_I2C_BUS_ARM},
-    {"name": "apds9250", "bus": RASP_I2C_BUS_ARM},
-    {"name": "veml7700", "bus": RASP_I2C_BUS_ARM},
+    {"name": "scd4x"},
+    {"name": "max31856"},
+    {"name": "sht35"},
+    {"name": "apds9250"},
+    {"name": "veml7700"},
     {"name": "veml6075", "bus": RASP_I2C_BUS_VC},
 ]
 
 
 def load_sensor():
     sensor_list = []
-    for sensor in SENSOR_MODULE_LIST:
-        logging.info("Load {name}".format(name=sensor["name"]))
-        mod = importlib.import_module("sensor." + sensor["name"])
-        sensor_list.append(getattr(mod, sensor["name"].upper())(bus=sensor["bus"]))
+    for sensor_module in SENSOR_MODULE_LIST:
+        logging.info("Load {name}".format(name=sensor_module["name"]))
+        mod = importlib.import_module("sensor." + sensor_module["name"])
+
+        if "bus" in sensor_module:
+            sensor = getattr(mod, sensor_module["name"].upper())(
+                bus=sensor_module["bus"]
+            )
+        else:
+            sensor = getattr(mod, sensor_module["name"].upper())()
+
+        sensor_list.append(sensor)
 
     return sensor_list
 
