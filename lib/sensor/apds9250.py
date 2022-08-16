a17b62f77a855d87691c6d39228a2dca3a2f0f93
KIMATA Tetsuya <kimata@green-rabbit.net>
2022-08-16 22:17:29 +0900

    style: コードを整形

--- lib/sensor/apds9250.py
+++ lib/sensor/apds9250.py
@@ -3,13 +3,13 @@
 
 # APDS-9250 を使って照度を取得するライブラリです．
 
-import time
 import struct
 import smbus
 
+
 class APDS9250:
-    NAME = 'APDS9250'
-    DEV_ADDR = 0x52 # 7bit
+    NAME = "APDS9250"
+    DEV_ADDR = 0x52  # 7bit
     RASP_I2C_BUS = 0x1  # Raspberry Pi の I2C のバス番号
 
     def __init__(self, bus=RASP_I2C_BUS, dev_addr=DEV_ADDR):
@@ -20,27 +20,26 @@ class APDS9250:
     def ping(self):
         try:
             data = self.i2cbus.read_byte_data(self.dev_addr, 0x06)
-            if (data & 0xF0)== 0xb0:
+            if (data & 0xF0) == 0xB0:
                 return True
             else:
                 return False
         except:
             return False
 
-
     def get_value(self):
         # Resolution = 20bit/400ms, Rate = 1000ms
         self.i2cbus.write_byte_data(self.dev_addr, 0x04, 0x05)
         # Gain = 1
         self.i2cbus.write_byte_data(self.dev_addr, 0x05, 0x01)
-        # Sensor = active 
+        # Sensor = active
         self.i2cbus.write_byte_data(self.dev_addr, 0x00, 0x02)
 
         data = self.i2cbus.read_i2c_block_data(self.dev_addr, 0x0A, 6)
-        ir = struct.unpack('<I', bytes(data[0:3]) + b'\x00')[0]
-        als = struct.unpack('<I', bytes(data[3:6]) + b'\x00')[0]
+        ir = struct.unpack("<I", bytes(data[0:3]) + b"\x00")[0]
+        als = struct.unpack("<I", bytes(data[3:6]) + b"\x00")[0]
 
-        if (als > ir):
+        if als > ir:
             return als * 46.0 / 400
         else:
             return als * 35.0 / 400
@@ -48,10 +47,10 @@ class APDS9250:
     def get_value_map(self):
         value = self.get_value()
 
-        return { 'lux': value }
+        return {"lux": value}
 
-    
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # TEST Code
     import os
     import sys
@@ -64,7 +63,7 @@ if __name__ == '__main__':
     apds9250 = sensor.apds9250.APDS9250()
 
     ping = apds9250.ping()
-    print('PING: %s' % ping)
+    print("PING: %s" % ping)
 
-    if (ping):
+    if ping:
         pprint.pprint(apds9250.get_value_map())
